4900583e66e666fc884ac60ca2f098d4ba733991
Ashil Farahmand <ashil.farahmand@gmail.com>
2019-01-23 22:24:55 -0500

    Modified entry file to render on root element

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+// import configureStore from './store/store';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>React is working!!!</h1>, root);
+  // let store;
+  // let preloadedState = {};
+
+  // store = configureStore(preloadedState);
+
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+
+  // ReactDOM.render(<Root store={ store } />, root);
+  ReactDOM.render(<Root />, root);
 });
