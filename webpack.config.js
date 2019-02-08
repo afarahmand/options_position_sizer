1e6c8e6ecfec24856be1fd7192fdbf65ac4b1dc6
Ashil Farahmand <ashil.farahmand@gmail.com>
2019-02-08 18:39:58 -0500

    Added UglifyJS and updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,61 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': { 'NODE_ENV': JSON.stringify('production') }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: true }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+// include plugins config
 module.exports = {
   context: __dirname,
-  entry: { main: './frontend/entry.jsx' },
+  entry: "./frontend/entry.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/react', '@babel/env']
+        }
       }
     ]
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: [".js", ".jsx", "*"]
   },
-  output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
   }
 };
-
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// plugins: [
-//   new ExtractTextPlugin({ filename: 'bundle.css' })
-// ],
