268b08d0a338daf72726fe95d3796cc4c6fcd214
Ashil Farahmand <ashil.farahmand@gmail.com>
2019-02-10 18:29:58 -0500

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,6 @@ var devPlugins = []; // if using any plugins for development
 var prodPlugins = [
   new webpack.DefinePlugin({
     'process.env': { 'NODE_ENV': JSON.stringify('production') }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: { warnings: true }
   })
 ];
 
